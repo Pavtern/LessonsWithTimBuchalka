f7d077b3e393f6989fa059678702b822fe12cf9f
terna <ternavskii.pavel@gmail.com>
2021-03-18 12:38:18 +0200

    Collections sort and copy

--- src/main/java/Collections/ArrayList/Main.java
+++ src/main/java/Collections/ArrayList/Main.java
@@ -1,9 +1,40 @@
 package Collections.ArrayList;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Theatre theatre = new Theatre("Fake", 3, 3);
-        theatre.getSeats();
-        theatre.reserveSeat("A03");
+        Theatre theatre = new Theatre("Fake", 8, 12);
+        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+        printList(seatCopy);
+
+        Collections.shuffle(seatCopy);
+        System.out.println("Printing seatCopy ");
+        printList(seatCopy);
+        System.out.println("Printing theatre.seats");
+        printList(theatre.seats);
+        sortList(seatCopy);
+        printList(seatCopy);
+
+    }
+
+    public static void printList(List<Theatre.Seat> list){
+        for(Theatre.Seat seat : list){
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("=================================================================================");
+    }
+
+    public static void sortList(List<? extends Theatre.Seat> list){
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                if(list.get(i).compareTo(list.get(j)) > 0){
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
     }
 }
