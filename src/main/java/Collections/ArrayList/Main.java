2166bb9709c71252938d899f2cc50cc9143a6a1d
terna <ternavskii.pavel@gmail.com>
2021-03-18 14:03:19 +0200

    Comparator

--- src/main/java/Collections/ArrayList/Main.java
+++ src/main/java/Collections/ArrayList/Main.java
@@ -7,34 +7,24 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Fake", 8, 12);
-        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
-        printList(seatCopy);
-
-        Collections.shuffle(seatCopy);
-        System.out.println("Printing seatCopy ");
-        printList(seatCopy);
-        System.out.println("Printing theatre.seats");
-        printList(theatre.seats);
-        sortList(seatCopy);
-        printList(seatCopy);
+        List<Theatre.Seat> seats = new ArrayList<>(theatre.getSeats());
+        Collections.sort(seats, Theatre.PRICE_ORDER);
+        printList(seats);
 
     }
 
     public static void printList(List<Theatre.Seat> list){
+        double price = 7;
         for(Theatre.Seat seat : list){
-            System.out.print(" " + seat.getSeatNumber());
+            if(price < seat.getPrice()){
+                price = seat.getPrice();
+                System.out.println();
+            }
+            System.out.print(" " + seat.getSeatNumber() + " "+ seat.getPrice() + "$");
         }
         System.out.println();
         System.out.println("=================================================================================");
     }
 
-    public static void sortList(List<? extends Theatre.Seat> list){
-        for (int i = 0; i < list.size() - 1; i++) {
-            for (int j = i+1; j < list.size(); j++) {
-                if(list.get(i).compareTo(list.get(j)) > 0){
-                    Collections.swap(list, i, j);
-                }
-            }
-        }
-    }
+
 }
