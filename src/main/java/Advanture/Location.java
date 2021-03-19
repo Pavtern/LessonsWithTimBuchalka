a36e2ebe30d19a965fca3be6188508bcf59923c9
terna <ternavskii.pavel@gmail.com>
2021-03-19 20:32:59 +0200

    Adventure edited

--- src/main/java/Advanture/Location.java
+++ src/main/java/Advanture/Location.java
@@ -8,16 +8,17 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationId, String description) {
+    public Location(int locationId, String description, Map<String, Integer> exits) {
         this.locationId = locationId;
         this.description = description;
-        exits = new HashMap<>();
-        exits.put("Q", 0);
+        if(exits != null){
+            this.exits = new HashMap<>(exits);
+        } else {
+            this.exits = new HashMap<>();
+        }
+        this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location){
-        exits.put(direction, location);
-    }
 
     public int getLocationId() {
         return locationId;
