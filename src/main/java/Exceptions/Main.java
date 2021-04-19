ebe7bd318be3c1f3dc9e1f5bf9affbc22870550c
terna <ternavskii.pavel@gmail.com>
2021-04-19 18:19:07 +0300

    Exceptions 2

--- src/main/java/Exceptions/Main.java
+++ src/main/java/Exceptions/Main.java
@@ -1,13 +1,54 @@
 package Exceptions;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int x = 98;
-        int y = 0;
-        System.out.println(divideEAFP(x,y));
-        System.out.println(divideLBYL(x,y));
-        System.out.println(divide(x,y));
+//        int x = 98;
+//        int y = 0;
+//        System.out.println(divideEAFP(x,y));
+//        System.out.println(divideLBYL(x,y));
+//        System.out.println(divide(x,y));
+
+        int x = getIntEAFP();
+        System.out.println("x is " + x);
+        x = getIntEAFP();
+        System.out.println("x is " + x);
+    }
+
+    private static int getInt(){
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+
+    private static int getIntLBYL(){
+        Scanner s = new Scanner(System.in);
+        boolean isValid = true;
+        System.out.println("Please enter a number:");
+        String input = s.next();
+        for (int i = 0; i < input.length(); i++) {
+            if(!Character.isDigit(input.charAt(i))){
+                isValid = false;
+                break;
+            }
+        }
+        if(isValid){
+            return Integer.parseInt(input);
+        }
+        return 0;
+    }
+
+    private static int getIntEAFP(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        try {
+            return s.nextInt();
+        } catch (InputMismatchException e){
+            return 0;
+        }
     }
 
     private static int divideLBYL(int x, int y){
