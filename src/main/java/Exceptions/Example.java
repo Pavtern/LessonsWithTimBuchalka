6933e1e67cc1615798e3a5f04f9491d56883b638
terna <ternavskii.pavel@gmail.com>
2021-04-26 18:21:42 +0300

    multi catch

--- src/main/java/Exceptions/Example.java
+++ src/main/java/Exceptions/Example.java
@@ -10,29 +10,27 @@ public class Example {
         System.out.println(result);
     }
 
-    private static int divide(){
+    private static int divide() {
         int x, y;
-        try{
+        try {
             x = getInt();
             y = getInt();
-        } catch (NoSuchElementException e){
-            throw new ArithmeticException("no suitable input");
-        }
-        System.out.println("x is " + x + ", y is " + y);
-        try {
+            System.out.println("x is " + x + ", y is " + y);
             return x / y;
-        } catch (ArithmeticException e){
+        } catch (NoSuchElementException e) {
+            throw new ArithmeticException("no suitable input");
+        } catch (ArithmeticException e) {
             throw new ArithmeticException("attempt to divide by zero");
         }
     }
 
-    private static int getInt(){
+    private static int getInt() {
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter an integer");
-        while (true){
-            try{
+        while (true) {
+            try {
                 return s.nextInt();
-            } catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 s.nextLine();
                 System.out.println("Please enter a number using only the digits 0 to 9");
             }
